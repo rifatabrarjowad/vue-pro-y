7c94d4a69321ed990eb311c209eb989a68b098d0
RIFAT ABRAR JOWAD <rajdiprifat2004@gmail.com>
2021-07-06 21:51:21 +0600

    adding file

--- hello-world/src/components/Home.vue
+++ hello-world/src/components/Home.vue
@@ -1,31 +1,16 @@
 <template>
-  <div>
+  <div class="home">
     <h1>Hello From Home component</h1>
-    <button @click="hello">Hello from home</button>
-    <h1 v-if="show">if</h1>
-    <h1 v-else>else</h1>
-    <button v-on:click="display">toogle</button>
   </div>
 </template>
 <script>
 export default {
   name: "Home",
-  props: {
-    data: String,
-    msg: String,
-  },
-  methods: {
-    hello() {
-      console.warn("alert from hello funtion");
-    },
-    display() {
-      this.show = !this.show;
-    },
-  },
-  data() {
-    return {
-      show: false,
-    };
-  },
 };
 </script>
+<style scoped>
+.home {
+  background-color: aquamarine;
+  height: 500px;
+}
+</style>>
